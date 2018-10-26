693feafc00703607f4e78abdf8ddb0e90550de20
Karol Majek <karolmajek@gmail.com>
2018-10-26 09:56:24 +0200

    fix forx problem

--- scripts/createScreenScripts.py
+++ scripts/createScreenScripts.py
@@ -14,7 +14,7 @@ for i, d in enumerate(data):
     d=d.replace('\n','')
     print(d,nodes[node_i])
     with open("105/%03d.sh"%node_i,'a') as f:
-        f.write("python3 /home/karolmajek/tensorflow/models/research/object_detection/model_tpu_main.py --pipeline_config_path=%spipeline.config --model_dir=%s --num_train_steps=50000 --sample_1_of_n_eval_examples=1 --alsologtostderr --tpu_name=%s &> logs/105_20181026/%s_%08d\n"%(d,d.replace('configs','logs/105_20181026/'+nodes[node_i]),nodes[node_i],nodes[node_i],i))
+        f.write("python3 /home/karolmajek/tensorflow/models/research/object_detection/model_tpu_main.py --pipeline_config_path=%spipeline.config --model_dir=%s --num_train_steps=50000 --sample_1_of_n_eval_examples=1 --alsologtostderr --tpu_name=%s 2>&1 > logs/105_20181026/%s_%08d\n"%(d,d.replace('configs','logs/105_20181026/'+nodes[node_i]),nodes[node_i],nodes[node_i],i))
 with open("runscreens.sh",'w') as f:
     for i in range(105):
         f.write("screen -S %s -dm bash -c \"sh ./105/%03d.sh\"\n"%(nodes[i],i))
